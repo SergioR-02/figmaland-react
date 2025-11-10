89b82210e8ba4d0a734b7a6971d03b682702adff
Sergio Ruiz <seruizh@unal.edu.co>
2025-11-10 00:07:05 -0500

    feat: create Header component and update Hero styles for improved layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
-import Navbar from './Navbar/Navbar';
-import Hero from './Hero/Hero';
+import Navbar from '../Navbar/Navbar';
+import Hero from '../Hero/Hero';
 
 const Header = () => {
   return (
